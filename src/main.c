ca1747f4392c838ff15e2927bcbd57079a5e56ed
vde-frei <vde-frei@student.42sp.org.br>
2023-11-03 01:14:14 -0300

    feat: started implementing fork pipe Changes to be committed: 	new file:   pipex 	modified:   src/main.c

--- src/main.c
+++ src/main.c
@@ -1,13 +1,54 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.c                                             :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: vde-frei <vde-frei@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/02 23:25:32 by vde-frei          #+#    #+#             */
+/*   Updated: 2023/11/03 01:13:31 by vde-frei         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "../includes/pipex.h"
 
-int	main(int argc, char **argv)
+int main(int argc, char **argv, char const *envp[])
 {
+	int		fd[2];
+	int		pid1;
+	int		pid2;
+	int		pid3;
+	char	**args;
+	char	*path;
+
 	(void)argv;
-	if (5 == argc)
+
+	if (5 != argc)
 	{
-		ft_printf("%s\n", "In");
-	}
-	else
 		ft_printf("%s\n", "Not In");
+		return (0);
+	}
+	if (pipe(fd) == -1)
+		return (1);
+	pid1 = fork();
+	if (pid1 < 0)
+		return (2);
+	if (pid1 == 0)
+	{
+		pid3 = fork();
+		if (pid3 == -1)
+			return (3);
+		// Usar strnstr para verificar o retorno
+		// guardar o path retornado na variabel path
+		// fazer o execve executar o path
+		execve(path, argv[3], envp);
+		
+		dup2(fd[1], STDOUT_FILENO);
+		close(0);
+		close(1);
+	}
+	pid_t	pid;
+
+
 	return (0);
 }
