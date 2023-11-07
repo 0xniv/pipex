d36e5cca431ee716cb4d3cd9d8f7771a23141d66
Vinicius Pereira <niviciusdev@gmail.com>
2023-11-07 16:18:48 -0300

    feat: norminetted and removed all comments from files  Changes to be committed: 	modified:   src/string.c

--- src/string.c
+++ src/string.c
@@ -6,32 +6,11 @@
 /*   By: nivi <nivi@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/04 17:35:21 by vde-frei          #+#    #+#             */
-/*   Updated: 2023/11/07 14:37:07 by nivi             ###   ########.fr       */
+/*   Updated: 2023/11/07 16:18:11 by nivi             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
-
-// static int	count_word(const char *s, char c)
-// {
-// 	int	index;
-// 	int	words;
-
-// 	index = 0;
-// 	words = 0;
-// 	while (s[index] != '\0')
-// 	{
-// 		if (s[index] != c)
-// 		{
-// 			words++;
-// 			while (s[index] != c && s[index] != '\0')
-// 				index++;
-// 		}
-// 		else
-// 			index++;
-// 	}
-// 	return (words);
-// }
 
 char	**get_paths(char **path, char **envp)
 {
